9948abbc550c4990251378fd5b2229d87b813b34
Pluto <1799718517@qq.com>
2024-04-28 21:46:53 +0800

    3

--- thread2.c
+++ thread2.c
@@ -3,147 +3,175 @@
 #include<pthread.h>
 #include<time.h>
 
-#define MATRIX_MAX 4
-
-struct myqueue{
-    int ***matrix;
-    int front;
-    int rear;
-};
+#define MAX_TASKS 100
+#define MAX_MATRIX 4
+#define MAX_THREADS 10
 
 typedef struct{
-    pthread_t *tid;
+    int ***matrix;
+    int **nmatrix;
+    int row;
+}Task;
 
-}thread_pool;
+Task task_queue[MAX_TASKS];
+int task_count=0;
+int front;
+int rear;
 
+void submit_task(Task *arg);
+void *calculate(void *arg);
 int main()
 {
-    struct myqueue *queue=(struct myqueue *)malloc(sizeof(struct myqueue));
-    queue->matrix=(int ***)malloc(sizeof(int**)*MATRIX_MAX);
-    int row;
+    Task *arg=(Task *)malloc(sizeof(Task));
     printf("请输入矩阵的行列数：\n");
-    scanf("%d",&row);
-    for(int i=0;i<MATRIX_MAX;i++)
+    scanf("%d",&arg->row);
+
+    //为计算前的两个矩阵分配内存
+    arg->matrix=(int ***)malloc(sizeof(int**)*2);
+    for(int i=0;i<2;i++)
     {
-        queue->matrix[i]=(int**)malloc(sizeof(int*)*row);
+        arg->matrix[i]=(int**)malloc(sizeof(int*)*arg->row);
     }
-    for(int i=0;i<MATRIX_MAX;i++)
+    for(int i=0;i<2;i++)
     {
-        for(int j=0;j<row;j++)
+        for(int j=0;j<arg->row;j++)
         {
-            queue->matrix[i][j]=(int *)malloc(sizeof(int)*row);
+            arg->matrix[i][j]=(int *)malloc(sizeof(int)*arg->row);
         }
     }
-    
-    //初始化矩阵
-    srand(time(NULL));
-    for(int i=0;i<MATRIX_MAX;i++)
+
+    //为计算后的矩阵分配内存
+    arg->nmatrix=(int **)malloc(sizeof(int*)*arg->row);
+    for(int i=0;i<arg->row;i++)
     {
-        for(int j=0;j<row;j++)
-        {
-            for(int k=0;k<row;k++)
-            {
-                queue->matrix[i][j][k]=rand()%10;
-            }
-        }
+        arg->nmatrix[i]=(int*)malloc(sizeof(int)*arg->row);
     }
 
     //输出计算前矩阵
-    for(int i=0;i<MATRIX_MAX;i++)
+    for(int i=0;i<2;i++)
     {
         printf("第%d个矩阵:\n",i+1);
-        for(int j=0;j<row;j++)
+        for(int j=0;j<arg->row;j++)
         {
-            for(int k=0;k<row;k++)
+            for(int k=0;k<arg->row;k++)
             {
-                printf("%4d",queue->matrix[i][j][k]);
+                printf("%4d",arg->matrix[i][j][k]);
             }
             printf("\n");
         }
         printf("\n");
     }
 
-    //矩阵相乘
-    int ***nmatrix=(int ***)malloc(sizeof(int**)*MATRIX_MAX/2);
-    for(int i=0;i<MATRIX_MAX/2;i++)
+    //创建线程计算矩阵相乘
+    pthread_t tid[MAX_THREADS];
+    for(int i=0;i<MAX_THREADS;i++)
     {
-        nmatrix[i]=(int**)malloc(sizeof(int*)*row);
+        pthread_create(&tid[i],NULL,calculate,NULL);
     }
-    for(int i=0;i<MATRIX_MAX/2;i++)
+    for(int i=0;i<10;i++)
     {
-        for(int j=0;j<row;j++)
+        srand(time(NULL));
+        for(int i=0;i<2;i++)
         {
-            nmatrix[i][j]=(int *)malloc(sizeof(int)*row);
-        }
-    }
-    int value=0;
-    int len=0;
-    int count=0;
-    for(int a=0;a<MATRIX_MAX-1;a++)
-    {
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<row;j++)
+            for(int j=0;j<arg->row;j++)
             {
-                for(int k=0;k<row;k++)
+                for(int k=0;k<arg->row;k++)
                 {
-                    //行、列 元素的乘积之和 
-                    value+=queue->matrix[a][i][k]*queue->matrix[a+1][k][j];
-                }
-                nmatrix[count][i][len++]=value;
-                value=0;
-                //len==a1 指的是二维数组已经到达当行的最后元素
-                if(len==row)
-                {
-                    len=0;
+                    arg->matrix[i][j][k]=rand()%10;
                 }
             }
         }
-        a++;
-        count++;
+        submit_task(arg);
     }
+
 
     //输出计算后矩阵
-    printf("两两相乘得：\n");
-    for(int i=0;i<MATRIX_MAX/2;i++)
-    {
-        printf("第%d个结果:\n",i+1);
-        for(int j=0;j<row;j++)
-        {
-            for(int k=0;k<row;k++)
-            {
-                printf("%4d",nmatrix[i][j][k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
+    // printf("两两相乘得：\n");
+    // for(int i=0;i<MAX_MATRIX/2;i++)
+    // {
+    //     printf("第%d个结果:\n",i+1);
+    //     for(int j=0;j<arg->row;j++)
+    //     {
+    //         for(int k=0;k<arg->row;k++)
+    //         {
+    //             printf("%4d",arg->nmatrix[i][j][k]);
+    //         }
+    //         printf("\n");
+    //     }
+    //     printf("\n");
+    // }
     
-    for(int i=0;i<MATRIX_MAX/2;i++)
+    //释放计算后矩阵内存
+    for(int i=0;i<arg->row;i++)
     {
-        for(int j=0;j<row;j++)
+        free(arg->nmatrix[i]);
+    }
+    free(arg->nmatrix);
+
+    //释放计算前两个矩阵的内存
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<arg->row;j++)
         {
-            free(nmatrix[i][j]);
+            free(arg->matrix[i][j]);
         }
     }
-    for(int i=0;i<MATRIX_MAX/2;i++)
+    for(int i=0;i<2;i++)
     {
-        free(nmatrix[i]);
+        free(arg->matrix[i]);
     }
-    free(nmatrix);
-    for(int i=0;i<MATRIX_MAX;i++)
-    {
-        for(int j=0;j<row;j++)
-        {
-            free(queue->matrix[i][j]);
-        }
-    }
-    for(int i=0;i<MATRIX_MAX;i++)
-    {
-        free(queue->matrix[i]);
-    }
-    free(queue->matrix);
-    free(queue);
+    free(arg->matrix);
+
+    free(arg);
 
     return 0;
+}
+void *calculate(void *arg)
+{
+    while(1)
+    {
+        int value=0;
+        int len=0;
+        Task task;
+        if(front!=rear)
+        {
+            task=task_queue[front];
+            front=(front+1)%MAX_TASKS;
+            
+            for(int i=0;i<task.row;i++)
+            {
+                for(int j=0;j<task.row;j++)
+                {
+                    for(int k=0;k<task.row;k++)
+                    {
+                        //行、列 元素的乘积之和 
+                        value+=task.matrix[0][i][k]*task.matrix[1][k][j];
+                    }
+                    task.nmatrix[i][len++]=value;
+                    value=0;
+                    //len==a1 指的是二维数组已经到达当行的最后元素
+                    if(len==task.row)
+                    {
+                        len=0;
+                    }
+                }
+            }
+        }
+        else
+        {
+            // 如果没有任务可执行，线程休眠一段时间
+            usleep(10000);
+        }
+    }
+    return NULL;
+}
+void submit_task(Task *arg)
+{
+    if(task_count<MAX_TASKS)
+    {
+        // 将任务添加到队列尾部
+        task_queue[rear]=*arg;
+        rear=(rear+1)%MAX_TASKS;
+        task_count++;
+    }
 }
