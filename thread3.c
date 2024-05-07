d1f0b0eb843538d94ca94d82f7072807a4f367ec
Pluto <1799718517@qq.com>
2024-05-07 21:27:42 +0800

    2024.5.7

--- thread3.c
+++ thread3.c
@@ -14,26 +14,32 @@ struct SPSCQueue {
 } typedef SPSCQueue;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_full = PTHREAD_COND_INITIALIZER;
 
 SPSCQueue *SPSCQueueInit(int capacity);
-void SPSCQueuePush(SPSCQueue *queue, void *s);
+void SPSCQueuePush(SPSCQueue *queue, int s);
 void *SPSCQueuePop(SPSCQueue *queue);
 void SPSCQueueDestory(SPSCQueue *queue);
 void *producter(void *arg);
 void *consumer(void *arg);
-int main(void)
+int main()
 {
     srand(time(NULL));
-    pthread_mutex_init(&mutex,NULL);
-    pthread_cond_init(&cond, NULL);
+
+    SPSCQueue *queue=SPSCQueueInit(MAX_BUF);
+    
     pthread_t tid1,tid2;
-    pthread_create(&tid1,NULL,producter,NULL);
-    pthread_create(&tid2,NULL,consumer,NULL);
-    pthread_join(&tid1,NULL);
-    pthread_join(&tid2,NULL);
+    pthread_create(&tid1,NULL,producter,queue);
+    pthread_create(&tid2,NULL,consumer,queue);
+
+    pthread_join(tid1,NULL);
+    pthread_join(tid2,NULL);
+
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
+    pthread_cond_destroy(&cond_empty);
+    pthread_cond_destroy(&cond_full);
+    SPSCQueueDestory(queue);
 }
 SPSCQueue *SPSCQueueInit(int capacity)
 {
@@ -44,9 +50,9 @@ SPSCQueue *SPSCQueueInit(int capacity)
     queue->rear=0;
     return queue;
 }
-void SPSCQueuePush(SPSCQueue *queue, void *s)
+void SPSCQueuePush(SPSCQueue *queue, int s)
 {
-    queue->value[queue->rear]=(int*)s;
+    queue->value[queue->rear]=s;
     queue->rear=(queue->rear+1)%MAX_BUF;
 }
 void *SPSCQueuePop(SPSCQueue *queue)
@@ -62,13 +68,33 @@ void *producter(void *arg)
 {
     while(1)
     {
-        SPSCQueuePush();
+        pthread_mutex_lock(&mutex); // 加锁
+        while(((SPSCQueue *)arg)->buf_count==MAX_BUF)
+        {
+            pthread_cond_wait(&cond_full,&mutex); 
+        }
+        SPSCQueuePush((SPSCQueue *)arg,rand()%10);
+        ((SPSCQueue *)arg)->buf_count++;
+        printf("product:%d\n",((SPSCQueue *)arg)->value[((SPSCQueue *)arg)->rear-1]);
+        pthread_mutex_unlock(&mutex); // 解锁
+        pthread_cond_signal(&cond_empty);
+        sleep(rand()%5);
     }
 }
 void *consumer(void *arg)
 {
     while(1)
     {
-
+        pthread_mutex_lock(&mutex); // 加锁
+        while(((SPSCQueue *)arg)->buf_count==0)
+        {
+            pthread_cond_wait(&cond_empty,&mutex); 
+        }
+        printf("consume:%d\n",((SPSCQueue *)arg)->value[((SPSCQueue *)arg)->front]);
+        SPSCQueuePop((SPSCQueue *)arg);
+        ((SPSCQueue *)arg)->buf_count--;
+        pthread_mutex_unlock(&mutex); // 解锁
+        pthread_cond_signal(&cond_full);
+        sleep(rand()%5);
     }
 }
---
+++ thread4.c
@@ -0,0 +1,180 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<time.h>
+#include<unistd.h>
+
+#define MAX_BUF 100
+
+struct MPMCQueue {
+    int *value;
+    int buf_count;
+    int front;
+    int rear;
+} typedef MPMCQueue;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_full = PTHREAD_COND_INITIALIZER;
+
+MPMCQueue *MPMCQueueInit(int capacity);
+void MPMCQueuePush(MPMCQueue *queue, void *s);
+void *MPMCQueuePop(MPMCQueue *queue);
+void MPMCQueueDestory(MPMCQueue *);
+void *producter1(void *arg);
+void *producter2(void *arg);
+void *producter3(void *arg);
+void *consumer1(void *arg);
+void *consumer2(void *arg);
+void *consumer3(void *arg);
+int main()
+{
+    srand(time(NULL));
+
+    MPMCQueue *queue=MPMCQueueInit(MAX_BUF);
+
+    pthread_t tid_pro1,tid_pro2,tid_pro3,tid_con1,tid_con2,tid_con3;
+    pthread_create(&tid_pro1,NULL,producter1,queue);
+    pthread_create(&tid_pro1,NULL,producter2,queue);
+    pthread_create(&tid_pro1,NULL,producter3,queue);
+    pthread_create(&tid_con1,NULL,consumer1,queue);
+    pthread_create(&tid_con2,NULL,consumer2,queue);
+    pthread_create(&tid_con3,NULL,consumer3,queue);
+
+    pthread_join(tid_pro1,NULL);
+    pthread_join(tid_pro2,NULL);
+    pthread_join(tid_pro3,NULL);
+    pthread_join(tid_con1,NULL);
+    pthread_join(tid_con2,NULL);
+    pthread_join(tid_con3,NULL);
+
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond_empty);
+    pthread_cond_destroy(&cond_full);
+    SPSCQueueDestory(queue);
+}
+MPMCQueue *MPMCQueueInit(int capacity)
+{
+    MPMCQueue*queue=(MPMCQueue *)malloc(sizeof(MPMCQueue));
+    queue->value=(int*)malloc(sizeof(int)*capacity);
+    queue->buf_count=0;
+    queue->front=0;
+    queue->rear=0;
+    return queue;
+}
+void MPMCQueuePush(MPMCQueue *queue, void *s)
+{
+    queue->value[queue->rear]=s;
+    queue->rear=(queue->rear+1)%MAX_BUF;
+}
+void *MPMCQueuePop(MPMCQueue *queue)
+{
+    queue->front=(queue->front+1)%MAX_BUF;
+}
+void MPMCQueueDestory(MPMCQueue *queue)
+{
+    free(queue->value);
+    free(queue);
+}
+void *producter1(void *arg)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex); // 加锁
+        while(((MPMCQueue *)arg)->buf_count==MAX_BUF)
+        {
+            pthread_cond_wait(&cond_full,&mutex); 
+        }
+        SPSCQueuePush((MPMCQueue *)arg,rand()%10);
+        ((MPMCQueue *)arg)->buf_count++;
+        printf("product:%d\n",((MPMCQueue *)arg)->value[((MPMCQueue *)arg)->rear-1]);
+        pthread_mutex_unlock(&mutex); // 解锁
+        pthread_cond_signal(&cond_empty);
+        sleep(rand()%5);
+    }
+}
+void *producter2(void *arg)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex); // 加锁
+        while(((MPMCQueue *)arg)->buf_count==MAX_BUF)
+        {
+            pthread_cond_wait(&cond_full,&mutex); 
+        }
+        SPSCQueuePush((MPMCQueue *)arg,rand()%10);
+        ((MPMCQueue *)arg)->buf_count++;
+        printf("product:%d\n",((MPMCQueue *)arg)->value[((MPMCQueue *)arg)->rear-1]);
+        pthread_mutex_unlock(&mutex); // 解锁
+        pthread_cond_signal(&cond_empty);
+        sleep(rand()%5);
+    }
+}
+void *producter3(void *arg)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex); // 加锁
+        while(((MPMCQueue *)arg)->buf_count==MAX_BUF)
+        {
+            pthread_cond_wait(&cond_full,&mutex); 
+        }
+        SPSCQueuePush((MPMCQueue *)arg,rand()%10);
+        ((MPMCQueue *)arg)->buf_count++;
+        printf("product:%d\n",((MPMCQueue *)arg)->value[((MPMCQueue *)arg)->rear-1]);
+        pthread_mutex_unlock(&mutex); // 解锁
+        pthread_cond_signal(&cond_empty);
+        sleep(rand()%5);
+    }
+}
+void *consumer1(void *arg)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex); // 加锁
+        while(((MPMCQueue *)arg)->buf_count==0)
+        {
+            pthread_cond_wait(&cond_empty,&mutex); 
+        }
+        printf("consume:%d\n",((MPMCQueue *)arg)->value[((MPMCQueue *)arg)->front]);
+        SPSCQueuePop((MPMCQueue *)arg);
+        ((MPMCQueue *)arg)->buf_count--;
+        pthread_mutex_unlock(&mutex); // 解锁
+        pthread_cond_signal(&cond_full);
+        sleep(rand()%5);
+    }
+}
+void *consumer2(void *arg)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex); // 加锁
+        while(((MPMCQueue *)arg)->buf_count==0)
+        {
+            pthread_cond_wait(&cond_empty,&mutex); 
+        }
+        printf("consume:%d\n",((MPMCQueue *)arg)->value[((MPMCQueue *)arg)->front]);
+        SPSCQueuePop((MPMCQueue *)arg);
+        ((MPMCQueue *)arg)->buf_count--;
+        pthread_mutex_unlock(&mutex); // 解锁
+        pthread_cond_signal(&cond_full);
+        sleep(rand()%5);
+    }
+}
+void *consumer3(void *arg)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex); // 加锁
+        while(((MPMCQueue *)arg)->buf_count==0)
+        {
+            pthread_cond_wait(&cond_empty,&mutex); 
+        }
+        printf("consume:%d\n",((MPMCQueue *)arg)->value[((MPMCQueue *)arg)->front]);
+        SPSCQueuePop((MPMCQueue *)arg);
+        ((MPMCQueue *)arg)->buf_count--;
+        pthread_mutex_unlock(&mutex); // 解锁
+        pthread_cond_signal(&cond_full);
+        sleep(rand()%5);
+    }
+}
