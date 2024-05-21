543cc3b58a099a6b3bc317c310996a8c44c08dd0
Pluto <1799718517@qq.com>
2024-05-21 21:18:55 +0800

    2024.5.21

--- list_lock.c
+++ list_lock.c
@@ -13,14 +13,45 @@ void listInit(list_lock_t* list) {
 void producer(list_lock_t* list, DataType value) {
     while(1)
     {
-
+        pthread_mutex_lock(&list->mutex); // 加锁
+        LinkList node=(LinkList)malloc(sizeof(LNode));
+        LinkList p=list->head;
+        while(p->next!=NULL)
+        {
+            p=p->next;
+        }
+        node->value=value;
+        p->next=node;
+        node->next=NULL;
+        pthread_mutex_unlock(&list->mutex); // 解锁
+        pthread_cond_signal(&list->cond);
     }
 }
 
 void consumer(list_lock_t* list) {
-   perror("This function is not implemented");
+    while(1)
+    {
+        pthread_mutex_lock(&list->mutex); // 加锁
+        pthread_cond_wait(&list->cond,&list->mutex); 
+        LinkList p=list->head;
+        while(p->next->next!=NULL)
+        {
+            p=p->next;
+        }
+        LinkList tem=p->next;
+        free(tem);
+        p->next=NULL;
+        pthread_mutex_unlock(&list->mutex); // 解锁
+    }
 }
 
 int getListSize(list_lock_t* list) {
-  perror("This function is not implemented");
+    int n=0;
+    LinkList p=list->head;
+    while(p->next!=NULL)
+    {
+        p=p->next;
+        n++;
+    }
+    return n;
 }
